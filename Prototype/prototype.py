cb4bc00bfd6abde7642ac91b8b55ade31df3ff55
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2020-11-27 15:10:33 +0100

    prototype

---
+++ Prototype/prototype.py
@@ -0,0 +1,25 @@
+"""
+Specify the kinds of objects to create using a prototypical instance,
+and create new objects by copying this prototype.
+
+*if we have an object and we want to have a new object but with the same values.
+"""
+
+import copy
+
+
+class Prototype:
+    """
+    Example class to be copied.
+    """
+
+    pass
+
+
+def main():
+    prototype = Prototype()
+    prototype_copy = copy.deepcopy(prototype)
+
+
+if __name__ == "__main__":
+    main()
